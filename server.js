aaa0be3caa9e67e74c0df9c0357318b8e983f11b
spamalope01 <spamalope01@yahoo.com>
2016-11-22 15:18:27 -0800

    halfway through the exercise.

---
+++ server.js
@@ -0,0 +1,17 @@
+let http = require ('http');
+let url = require('url');
+
+function start(route, handle) {
+  function onRequest(req, res) {
+    let pathname = url.parse(req.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+     route(handle, pathname, res);
+  }
+
+   http.createServer(onRequest).listen(8888);
+   console.log("Server has started.");
+ }
+
+
+ exports.start = start;
